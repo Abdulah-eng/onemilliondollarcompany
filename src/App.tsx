63ffe2aaffde07a2bda8e6a1e4b26e7aa3972ce9
onemanbillioncompany <1manbillioncompany@gmail.com>
2025-08-16 10:07:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
+// src/App.tsx
+
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+
+// 1. We import our AuthProvider and the new pages
+import { AuthProvider } from "./contexts/AuthContext";
+import LandingPage from "./pages/LandingPage"; // Make sure this path is correct
+import GetStartedPage from "./pages/auth/GetStartedPage";
+import LoginPage from "./pages/auth/LoginPage";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+import NotFound from "./pages/NotFound"; // Make sure this path is correct
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      {/* 2. We wrap everything inside AuthProvider */}
+      <AuthProvider>
+        <Toaster richColors position="top-right" />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+
+            {/* 3. We add the new routes for authentication */}
+            <Route path="/get-started" element={<GetStartedPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+
+            {/* Your "Not Found" route must always be the last one */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
