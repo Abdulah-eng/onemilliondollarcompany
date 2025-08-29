29247cd955cfecf999feb0f8f51d7378f4c58fb1
onemanbillioncompany <1manbillioncompany@gmail.com>
2025-08-29 10:46:08 +0200

    Update testimonials.js

--- src/mockdata/landingpage/testimonials.js
+++ src/mockdata/landingpage/testimonials.js
@@ -1,8 +1,8 @@
-import avatar1 from '@/assets/avatars/avatar-1.webp';
-import avatar2 from '@/assets/avatars/avatar-2.webp';
-import avatar3 from '@/assets/avatars/avatar-3.webp';
-import avatar4 from '@/assets/avatars/avatar-4.webp';
-import avatar5 from '@/assets/avatars/avatar-5.webp';
+import avatar1 from '@/assets/avatar-1.webp';
+import avatar2 from '@/assets/avatar-2.webp';
+import avatar3 from '@/assets/avatar-3.webp';
+import avatar4 from '@/assets/avatar-4.webp';
+import avatar5 from '@/assets/avatar-5.webp';
 
 export const TESTIMONIALS = [
   {
