3cc57ba004733252bd350322b39fea94d3e18e7d
onemanbillioncompany <1manbillioncompany@gmail.com>
2025-09-06 13:23:32 +0200

    Update FitnessProgression.tsx

--- src/components/customer/progress/fitness/FitnessProgression.tsx
+++ src/components/customer/progress/fitness/FitnessProgression.tsx
@@ -173,7 +173,7 @@ export default function FitnessProgression({
               <ResponsiveContainer width="100%" height="100%">
                 <AreaChart
                   data={exerciseData.chartData}
-                  margin={{ top: 10, right: 0, left: 0, bottom: 0 }}
+                  margin={{ top: 10, right: -30, left: -30, bottom: 0 }} // stretch both sides
                 >
                   <defs>
                     <linearGradient id="colorFitness" x1="0" y1="0" x2="0" y2="1">
