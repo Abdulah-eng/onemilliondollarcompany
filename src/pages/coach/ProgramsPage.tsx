80ac9ba9e76b0d26e3488a963489e857acd0a48e
onemanbillioncompany <1manbillioncompany@gmail.com>
2025-09-17 17:06:08 +0200

    Update ProgramsPage.tsx

--- src/pages/coach/ProgramsPage.tsx
+++ src/pages/coach/ProgramsPage.tsx
@@ -1,15 +1,16 @@
-// src/pages/coach/ProgramsPage.tsx
 'use client';
 
 import React from 'react';
 import ProgramsHeader from '@/components/coach/programs/ProgramsHeader';
 import ProgramsList from '@/components/coach/programs/ProgramsList';
+import ProgramActionButton from '@/components/coach/programs/ProgramActionButton';
 
 const ProgramsPage = () => {
   return (
     <div className="flex flex-col gap-6 p-4 md:p-8">
       <ProgramsHeader />
       <ProgramsList />
+      <ProgramActionButton />
     </div>
   );
 };
